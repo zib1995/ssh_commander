180f432dbd2449011ef86730ea65845119025dbd
Konstantin Zibarev <zib1995@mail.ru>
2018-04-15 01:50:31 +0300

    Basic classis added.

--- main.py
+++ main.py
@@ -1,4 +1,28 @@
 
 
-print('ok')
+class user_interface:
+	def __init__(self):
+		print('No implemented.')
 
+class ssh_client:
+	def connection(self, host, user, password):
+		print('No implemented.')
+	def command(self, request):
+		print('No implemented.')
+
+class file_manager:
+	def get_view(self, source):
+		file_list = []
+		print('No implemented.')
+		return file_list
+	def command_copy(self, source, target):
+		print('No implemented.')
+	def command_move(self, source, target):
+		print('No implemented.')
+	def command_delete(self, target):
+		print('No implemented.')
+	def command_create_dir(self, target):
+		print('No implemented.')
+
+if __name__ == '__main__':
+	print('Ok.')
