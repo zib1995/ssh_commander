f9798f8ec574eca242bfe4e51d14fb640665a7d6
Константин Зибарев <zib1995@mail.ru>
2018-04-16 03:14:21 +0300

    Partially implemented client functionality.

--- main.py
+++ main.py
@@ -1,14 +1,31 @@
 
+import paramiko
 
 class user_interface:
 	def __init__(self):
 		print('No implemented.')
 
-class ssh_client:
-	def connection(self, host, user, password):
-		print('No implemented.')
+class remote_host:
+	def __init__(self):
+		self.client = paramiko.SSHClient()
+		self.client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+	def connection(self, host, user, secret, port = 22):
+		self.client.connect(hostname = host, username = user, password = secret, port = port)
+		self.transport = paramiko.Transport((host, port))
+		self.transport.connect(username = user, password = secret)
+		self.sftp = paramiko.SFTPClient.from_transport(self.transport)
 	def command(self, request):
-		print('No implemented.')
+		stdin, stdout, stderr = self.client.exec_command(request)
+		data = stdout.read() + stderr.read()
+		return data
+	def get_file(self, remotepath, localpath):
+		sftp.get(remotepath, localpath)
+	def put_file(self, remotepath, localpath):
+		sftp.put(remotepath, localpath)
+	def disconnection(self):
+		self.sftp.close()
+		self.transport.close()
+		self.client.close()
 
 class file_manager:
 	def get_view(self, source):
@@ -25,4 +42,18 @@ class file_manager:
 		print('No implemented.')
 
 if __name__ == '__main__':
+
+	#Test host:
+
+	host = '192.168.33.33'
+	user = 'vagrant'
+	secret = 'vagrant'
+	port = 22
+	
+	target_host = remote_host()
+	target_host.connection(host, user, secret)
+	print(target_host.command('pwd'))
+	target_host.disconnection()
+	
+
 	print('Ok.')
