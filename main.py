e5a21f79cd0241b7fe037028215808e9ca936f40
Константин Зибарев <zib1995@mail.ru>
2018-05-15 04:24:49 +0300

    The output of the shell in the GUI is added.

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 
+import sys
 import paramiko
 import threading
+from PyQt5 import QtWidgets
+import dialog_connection
+import main_window
+import time
 
 class user_interface:
 	def __init__(self):
 		print('No implemented.')
 
 class remote_host:
-	def __init__(self):
+	def __init__(self, terminal):
 		self.client = paramiko.SSHClient()
 		self.client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+		self.terminal = terminal
 		
 	def connection(self, host, user, secret, port = 22):
 		self.client.connect(hostname = host, username = user, password = secret, port = port)
@@ -19,17 +25,23 @@ class remote_host:
 		
 		self.shell = self.client.invoke_shell()
 		self.continue_shell_session = True
-		self.shell_output_thread = threading.Thread(target = self.shell_output)
-		self.shell_output_thread.daemon = True
-		self.shell_output_thread.start()
+		self.terminal.append('The connection to host ' + host + ' is established.')
 		
 	def command(self, request):
 		stdin, stdout, stderr = self.client.exec_command(request)
 		data = stdout.read() + stderr.read()
 		data = data.decode('utf-8')
 		return data
-		
+
 	def command_shell(self):
+		self.command_shell_thread = threading.Thread(target = self.commands)
+		self.shell_output_thread = threading.Thread(target = self.shell_output)
+		self.command_shell_thread.daemon = True
+		self.shell_output_thread.daemon = True
+		self.command_shell_thread.start()
+		self.shell_output_thread.start()
+
+	def commands(self):
 		while self.continue_shell_session:
 			cmd = input()
 			self.shell.send(cmd + '\n')
@@ -39,7 +51,11 @@ class remote_host:
 			data = bytes()
 			while self.shell.recv_ready():
 				data += self.shell.recv(1024)
-			print(data.decode('utf-8')[:-1], end = '')
+			data_text = data.decode('utf-8')[:-1]
+			print(data_text, end = '')
+			if data_text != '':
+				self.terminal.append(data_text)
+				time.sleep(1)
 
 	def get_file(self, remotepath, localpath):
 		sftp.get(remotepath, localpath)
@@ -65,6 +81,11 @@ class file_manager:
 		print('No implemented.')
 
 if __name__ == '__main__':
+	app = QtWidgets.QApplication(sys.argv)
+	MainWindow = QtWidgets.QMainWindow()
+	ui_main_window = main_window.Ui_MainWindow()
+	ui_main_window.setupUi(MainWindow)
+	MainWindow.show()
 
 	#Test host:
 
@@ -72,11 +93,13 @@ if __name__ == '__main__':
 	user = 'vagrant'
 	secret = 'vagrant'
 	port = 22
-	
-	target_host = remote_host()
+
+	target_host = remote_host(terminal = ui_main_window.textEdit_terminal)
+
 	target_host.connection(host, user, secret)
 	
 	target_host.command_shell()
 	#target_host.disconnection()
 
 	print('Ok.')
+	sys.exit(app.exec_())
