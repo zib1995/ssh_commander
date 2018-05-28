7cd5386f6aee8b76bad1a4eb8b7739bd79d0dcc2
Константин Зибарев <zib1995@mail.ru>
2018-05-29 02:03:59 +0300

    Updated the functionality of the file manager.

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 
+import os
 import sys
 import paramiko
 import threading
 from PyQt5 import QtWidgets
+from PyQt5 import QtCore
 from PyQt5.QtCore import Qt
 import dialog_connection
 import main_window
 import time
+
+
 
 class UserInterface:
 	def __init__(self):
@@ -15,16 +19,30 @@ class UserInterface:
 		self.ui_main_window = main_window.Ui_MainWindow()
 		self.ui_main_window.setupUi(self.MainWindow)
 		self.ui_main_window.statusbar.showMessage('Start.')
-		self.define_reactions()
 		self.MainWindow.show()
-		self.reaction_to_menu_connect()
 		self.command = ''
+
+		self.file_manager = FileManager()
+		self.ui_main_window.listView_remote_first.setModel(self.file_manager.remote_first_list)
+		self.ui_main_window.listView_remote_second.setModel(self.file_manager.remote_second_list)
+		self.ui_main_window.listView_local.setModel(self.file_manager.local_list)
+
+		self.reaction_to_menu_connect()
+		self.define_reactions()
+
+		self.refresh_editLine()
+		self.file_manager.refresh_lists()
 
 	def start(self):
 		sys.exit(self.app.exec_())
 
 	def get_terminal(self):
 		return self.ui_main_window.textEdit_terminal
+
+	def refresh_editLine(self):
+		self.ui_main_window.lineEdit_remote_first_path.setText(self.file_manager.get_remote_first_path())
+		self.ui_main_window.lineEdit_remote_second_path.setText(self.file_manager.get_remote_second_path())
+		self.ui_main_window.lineEdit_local_path.setText(self.file_manager.get_local_path())
 
 	def define_reactions(self):
 		self.ui_main_window.actionConnect.triggered.connect(self.reaction_to_menu_connect)
@@ -40,19 +58,31 @@ class UserInterface:
 		self.DialogConnection.exec_()
 
 	def reaction_to_dialog_connection_connect(self):
+		# Test host:
+		host = '192.168.33.33'
+		user = 'vagrant'
+		secret = 'vagrant'
+		self.ui_dialog_connection.lineEdit_host.setText(host)
+		self.ui_dialog_connection.lineEdit_user.setText(user)
+		self.ui_dialog_connection.lineEdit_password.setText(secret)
+
 		host = self.ui_dialog_connection.lineEdit_host.text()
 		user = self.ui_dialog_connection.lineEdit_user.text()
 		secret = self.ui_dialog_connection.lineEdit_password.text()
 
 		self.target_host = RemoteHost(terminal = self.get_terminal())
 
-		# Test host:
-		#host = '192.168.33.33'
-		#user = 'vagrant'
-		#secret = 'vagrant'
-
 		self.target_host.connection(host, user, secret)
 		self.target_host.command_shell()
+
+
+		#a = self.target_host
+
+		self.file_manager.set_remote_host(self.target_host)
+
+		print('----')
+		print(self.target_host.command('ls'))
+		print('----')
 		#target_host.disconnection()
 
 		self.DialogConnection.close()
@@ -124,7 +154,57 @@ class RemoteHost:
 		self.transport.close()
 		self.client.close()
 
+class FileListModel(QtCore.QAbstractListModel):
+	def __init__(self, data = [], parent = None):
+		QtCore.QAbstractListModel.__init__(self, parent)
+		self.file_list = data
+	def set_file_list(self, file_list):
+		self.beginResetModel()
+		self.file_list = file_list
+		self.endResetModel()
+		print('OK?')
+	def rowCount(self, parent):
+		return len(self.file_list)
+	def data(self, index, role):
+		if role == QtCore.Qt.ToolTipRole:
+			return 'Some.'
+		if role == QtCore.Qt.DisplayRole:
+			return self.file_list[index.row()]
+
 class FileManager:
+	def __init__(self):
+		list_example = ['file.txt', 'script.sh', 'tmp', 'model.log']
+		self.remote_first_list = FileListModel(list_example)
+		self.remote_second_list = FileListModel()
+		self.local_list = FileListModel()
+
+		self.remote_first_path = '/home'
+		self.remote_second_path = '/'
+		self.local_path = 'C:\\'
+		self.local_list.set_file_list(os.listdir(self.local_path))
+
+		self.remote_host = None
+
+	def get_remote_first_path(self):
+		return self.remote_first_path
+	def get_remote_second_path(self):
+		return self.remote_second_path
+	def get_local_path(self):
+		return self.local_path
+
+	def set_remote_host(self, remote_host):
+		self.remote_host = remote_host
+
+	def refresh_lists(self):
+		if self.remote_host:
+			self.remote_first_list = self.remote_host.command('ls ' + self.remote_first_path).split()
+			self.remote_second_list = self.remote_host.command('ls ' + self.remote_first_path).split()
+		else:
+			self.remote_first_list = []
+			self.remote_second_list = []
+		self.local_list.set_file_list(os.listdir(self.local_path))
+		print('ok')
+
 	def get_view(self, source):
 		file_list = []
 		print('No implemented.')
